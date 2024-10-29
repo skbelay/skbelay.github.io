c5074126c41f7266ecdf3ceab0c47eb5395ba40f
skbelay <sarahkbelay@gmail.com>
2024-10-29 18:12:15 -0400

    updates

--- Generative-Identity/script.js
+++ Generative-Identity/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const verticalLine = document.querySelector('.line-vertical');
     const horizontalLine = document.querySelector('.line-horizontal');
+    const verticalLine2 = document.querySelector('.line-vertical-2');
+    const horizontalLine2 = document.querySelector('.line-horizontal-2');
 
     function getRandomAngle() {
         return Math.floor(Math.random() * 360);
@@ -8,9 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function rotateLines() {
         const angle = getRandomAngle();
+        const angle2 = getRandomAngle();
 
         verticalLine.style.transform = `translateX(-50%) rotate(${angle}deg)`;
         horizontalLine.style.transform = `translateY(-50%) rotate(${angle}deg)`;
+        
+        verticalLine2.style.transform = `translateX(-50%) rotate(${angle2}deg)`;
+        horizontalLine2.style.transform = `translateY(-50%) rotate(${angle2}deg)`;
     }
 
     rotateLines();

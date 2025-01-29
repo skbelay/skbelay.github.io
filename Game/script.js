b01cb096ec51c6ea6bee1f701af3ba4739074778
skbelay <sarahkbelay@gmail.com>
2025-01-29 15:15:12 -0500

    Update script.js

--- Game/script.js
+++ Game/script.js
@@ -1,38 +1,41 @@
-// Get all cards
 const cards = document.querySelectorAll('.card');
+const winningTag = document.querySelector('p')
 
 let flippedCards = [];
 let matchedPairs = 0;
 
-// Add click handler to each card
+// add click handler to each card
 cards.forEach(card => {
     card.addEventListener('click', function() {
-        // Ignore if card is already flipped or we already have 2 cards flipped
+        // ignore if card is already flipped or we already have 2 cards flipped
         if (card.classList.contains('flipped') || flippedCards.length >= 2) return;
 
-        // Flip the card
+        // flip the card
         card.classList.add('flipped');
         flippedCards.push(card);
 
-        // If we have 2 cards flipped, check for match
-        if (flippedCards.length === 2) {
+        // if 2 cards are flipped, check for match
+        if (flippedCards.length == 2) {
             checkMatch();
         }
     });
 });
 
 function checkMatch() {
-    const [card1, card2] = flippedCards;
+    const card1 = flippedCards[0]
+    const card2 = flippedCards[1]
     const img1 = card1.querySelector('.card-back img').src;
     const img2 = card2.querySelector('.card-back img').src;
 
-    if (img1 === img2) {
-        // It's a match!
-        matchedPairs++;
+    if (img1 == img2) {
+        // match!
+        matchedPairs = matchedPairs + 1
         flippedCards = [];
-        
+        if (matchedPairs == 6) {
+            winningTag.innerHTML = "U got it!";
+        }
     } else {
-        // Not a match, flip cards back
+        // not a match, flip cards back
         setTimeout(() => {
             card1.classList.remove('flipped');
             card2.classList.remove('flipped');
